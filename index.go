b5d31a4a3bbf17b6f6943b348857bf4fd444a29a
cxbsoft <cxbsoft@bsot.cn>
2021-08-20 23:35:05 +0800

    新增resultMap

--- index.go
+++ index.go
@@ -24,12 +24,7 @@ func getMyIPV6() string {
 	return ""
 }
 
-type Config struct {
-	Name  string
-	Value string
-}
-
-func readJson(jsonPath string) {
+func readJson(jsonPath string) map[string]interface{} {
 	jsonFile, err := os.Open(jsonPath)
 	if err != nil {
 		fmt.Println(err)
@@ -38,26 +33,67 @@ func readJson(jsonPath string) {
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 	var config map[string]interface{}
 	json.Unmarshal([]byte(byteValue), &config)
-	fmt.Println(config)
+	return config
+}
+
+// 命令行结构体
+type Args struct {
+	ServiceName    string                 // DDNS服务商
+	Config         string                 // 配置
+	commandOptions []string               // 命令行参数
+	resultMap      map[string]interface{} // 命令行参数变成interface
 }
 
 // 对用户的命令行参数进行处理
 func commandLineProcess(args []string) {
 	arglen := len(args)
 	if arglen > 1 {
+		var config Args
 		commands := args[1:]
+		var resultMap map[string]interface{}
 		for i := 0; i < (arglen - 1); i++ {
 			spliceCommand := commands[i]
+			resultMap[commands[i]] = commands[i+1]
 			switch spliceCommand {
 			case "-service":
 				serviceName := commands[i+1]
-				fmt.Println(serviceName)
+				config.ServiceName = serviceName
+				config.commandOptions = commands
+				fmt.Println(config.ServiceName)
 				i = i + 1
 				break
 			}
 		}
+		config.resultMap = resultMap
 	} else {
 		userChoose()
+	}
+}
+
+// 判断数组中是否有一个元素
+func in(target string, str_array []string) bool {
+	for _, element := range str_array {
+		if target == element {
+			return true
+		}
+	}
+	return false
+}
+
+// 处理
+func mainProcess(config Args) {
+	switch config.ServiceName {
+	case "cloudflare":
+		localConfig := readJson("config.json")
+		commandOptions := config.commandOptions
+
+		if in("config", commandOptions) {
+
+		}
+
+		cf_config_string := config.Config
+		var cf_config map[string]interface{}
+		json.Unmarshal([]byte(cf_config_string), &cf_config)
 	}
 }
 
