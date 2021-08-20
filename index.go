4711520423723f750a29fef64b24cd686b082e91
cxbsoft <cxbsoft@bsot.cn>
2021-08-20 22:39:42 +0800

    init

--- index.go
+++ index.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"regexp"
+	"strings"
 )
 
+func getMyIPV6() string {
+	s, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, a := range s {
+		i := regexp.MustCompile(`(\w+:){7}\w+`).FindString(a.String())
+		if strings.Count(i, ":") == 7 {
+			return i
+		}
+	}
+	return ""
+}
+
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println("欢迎使用DDNS Tools")
+	ipv6 := getMyIPV6()
+	fmt.Println("您目前的ipv6:" + ipv6)
 }
