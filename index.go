b21dc354378a34565c494b12a40638d4e5012531
cxbsoft <cxbsoft@bsot.cn>
2021-08-20 23:09:29 +0800

    Command Process

--- index.go
+++ index.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net"
+	"os"
 	"regexp"
 	"strings"
 )
@@ -21,8 +24,51 @@ func getMyIPV6() string {
 	return ""
 }
 
+type Config struct {
+	Name  string
+	Value string
+}
+
+func readJson(jsonPath string) {
+	jsonFile, err := os.Open(jsonPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var config map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &config)
+	fmt.Println(config)
+}
+
+// 对用户的命令行参数进行处理
+func commandLineProcess(args []string) {
+	arglen := len(args)
+	if arglen > 1 {
+		commands := args[1:]
+		for i := 0; i < (arglen - 1); i++ {
+			spliceCommand := commands[i]
+			switch spliceCommand {
+			case "-service":
+				serviceName := commands[i+1]
+				fmt.Println(serviceName)
+				i = i + 1
+				break
+			}
+		}
+	} else {
+		userChoose()
+	}
+}
+
+func userChoose() {
+
+}
+
 func main() {
 	fmt.Println("欢迎使用DDNS Tools")
 	ipv6 := getMyIPV6()
 	fmt.Println("您目前的ipv6:" + ipv6)
+	//readJson("config.json")
+	commandLineProcess(os.Args)
 }
