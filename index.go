2bd265f4a2e6bdf0b3d92063c9323f92ddf38246
cxbsoft <cxbsoft@bsot.cn>
2021-08-21 00:17:20 +0800

    判断读取json

--- index.go
+++ index.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net"
 	"os"
 	"regexp"
@@ -50,23 +51,41 @@ func commandLineProcess(args []string) {
 	if arglen > 1 {
 		var config Args
 		commands := args[1:]
-		var resultMap map[string]interface{}
+		resultMap := make(map[string]interface{})
 		for i := 0; i < (arglen - 1); i++ {
 			spliceCommand := commands[i]
-			resultMap[commands[i]] = commands[i+1]
 			switch spliceCommand {
 			case "-service":
 				serviceName := commands[i+1]
 				config.ServiceName = serviceName
 				config.commandOptions = commands
+				resultMap["service"] = serviceName
 				fmt.Println(config.ServiceName)
 				i = i + 1
 				break
+			case "-config":
+				commandConfig := commands[i+1]
+				resultMap["config"] = commandConfig
+				i = i + 1
+				break
+
 			}
 		}
 		config.resultMap = resultMap
+		mainProcess(config)
 	} else {
 		userChoose()
+	}
+}
+
+//检查cloudflare配置是否存在
+func checkCloudflareConfig(jsonConfig map[string]interface{}) bool {
+	_, ok1 := jsonConfig["cloudflare"].(map[string]interface{})["email"]
+	_, ok2 := jsonConfig["cloudflare"].(map[string]interface{})["apikey"]
+	if ok1 && ok2 {
+		return true
+	} else {
+		return false
 	}
 }
 
@@ -85,16 +104,39 @@ func mainProcess(config Args) {
 	switch config.ServiceName {
 	case "cloudflare":
 		localConfig := readJson("config.json")
+		configExist := checkCloudflareConfig(localConfig)
 		commandOptions := config.commandOptions
-
-		if in("config", commandOptions) {
-
+		if in("-config", commandOptions) {
+			configContentString := config.resultMap["config"].(string)
+			var configContent map[string]interface{}
+			err := json.Unmarshal([]byte(configContentString), &configContent)
+			if err != nil {
+				log.Fatal("config.json解析失败")
+			}
+			_, ok1 := configContent["email"]
+			_, ok2 := configContent["apikey"]
+			if ok1 && ok2 {
+				fmt.Println("合法")
+			} else {
+				fmt.Println("不合法")
+			}
+		} else {
+			if configExist {
+				email := localConfig["cloudflare"].(map[string]interface{})["email"].(string)
+				apikey := localConfig["cloudflare"].(map[string]interface{})["apikey"].(string)
+				cloudflareChangeDns(email, apikey, commandOptions, "Asd")
+			}
 		}
 
 		cf_config_string := config.Config
 		var cf_config map[string]interface{}
 		json.Unmarshal([]byte(cf_config_string), &cf_config)
 	}
+}
+
+func cloudflareChangeDns(email string, apikey string, targets []string, target_ip string) {
+	fmt.Println(email)
+	fmt.Println(apikey)
 }
 
 func userChoose() {
